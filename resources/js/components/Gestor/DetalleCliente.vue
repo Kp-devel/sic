07df6319f97f04914c2c599719b8e5fdb1d9ba2d
Kp-devel <kafer2515@gmail.com>
2020-08-30 16:18:44 -0500

    actualizacion detalle cliente

--- resources/js/components/Gestor/DetalleCliente.vue
+++ resources/js/components/Gestor/DetalleCliente.vue
@@ -4,51 +4,51 @@
             <table width="100%" v-if="infoCliente">
                 <tr class="font-12"> 
                     <td class="text-right pr-1">Código</td>
-                    <td class=""><label class="form-control font-12 form-control-sm mb-1">{{datos[0].codigo}}</label></td>
+                    <td class=""><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{datos[0].codigo?datos[0].codigo:'-'}}</label></td>
                     <td class="font-11 pr-1 text-right">DNI/RUC</td>
-                    <td><label class="form-control font-12 form-control-sm mb-1">{{datos[0].dni}}</label></td>
+                    <td><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{datos[0].dni?datos[0].dni:'-'}}</label></td>
                 </tr>
                 <tr class="font-12"> 
                     <td class="text-right pr-1">Dirección</td>
-                    <td colspan="3"><label class="form-control font-12 form-control-sm mb-1">{{infoCliente.direccion}}</label></td>
+                    <td colspan="3"><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.direccion?infoCliente.direccion:'-'}}</label></td>
                 </tr>
                 <tr class="font-12"> 
                     <td class="text-right pr-1">Dist.</td>
-                    <td><label class="form-control font-12 form-control-sm mb-1">{{infoCliente.distrito}}</label></td>
+                    <td><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.distrito?infoCliente.distrito:'-'}}</label></td>
                     <td class="text-right pr-1">Prov.</td>
-                    <td><label class="form-control font-12 form-control-sm mb-1">{{infoCliente.provincia}}</label></td>
+                    <td><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.provincia?infoCliente.provincia:'-'}}</label></td>
                 </tr>
                 <tr class="font-12"> 
                     <td class="text-right pr-1">Depto.</td>
-                    <td><label class="form-control font-12 form-control-sm mb-1">{{infoCliente.departamento}}</label></td>
+                    <td><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.departamento?infoCliente.departamento:'-'}}</label></td>
                     <td class="text-right pr-1">Prioridad</td>
-                    <td><label class="form-control font-12 form-control-sm mb-1"></label></td>
+                    <td><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">-</label></td>
                 </tr>
                 <tr class="font-12"> 
                     <td class="text-right pr-1">C. Laboral</td>
-                    <td colspan="3"><label class="form-control font-12 form-control-sm mb-1">{{infoCliente.laboral}}</label></td>
+                    <td colspan="3"><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.laboral?infoCliente.laboral:'-'}}</label></td>
                 </tr>
                 <tr class="font-12"> 
                     <td class="text-right pr-1">Sueldo</td>
                     <td colspan="3">
                         <div class="d-flex justify-content-between">
                             <div class="d-flex">
-                                <label class="form-control font-12 form-control-sm mb-1">{{infoCliente.sueldo? 'S/.'+infoCliente.sueldo:'-' }}</label>
+                                <label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.sueldo? 'S/.'+infoCliente.sueldo:'-' }}</label>
                             </div>
                             <div class="d-flex">
                                 <label class="px-1 pt-2">Entidades</label>
-                                <label class="form-control font-12 form-control-sm mb-1">{{infoCliente.entidades? infoCliente.entidades:'-'}}</label>
+                                <label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.entidades? infoCliente.entidades:'-'}}</label>
                             </div>
                             <div class="d-flex">
                                 <label class="px-1 pt-2">Score</label>
-                                <label class="form-control font-12 form-control-sm mb-1">{{infoCliente.score? infoCliente.score:'-'}}</label>
+                                <label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{infoCliente.score? infoCliente.score:'-'}}</label>
                             </div>
                         </div>
                     </td>
                 </tr>
                 <tr class="font-12"> 
                     <td class="text-right pr-1">Email</td>
-                    <td colspan="3"><label class="form-control font-12 form-control-sm mb-1">{{datos[0].email}}</label></td>
+                    <td colspan="3"><label class="form-control font-12 form-control-sm mb-1 w-100 h-100">{{datos[0].email?datos[0].email:'-'}}</label></td>
                 </tr>
             </table>
         </div>
